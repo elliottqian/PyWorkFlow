e9ddb2efc8484017fd62194a7e38d0dc43ec7d57
qianwei <752004979@qq.com>
2017-08-08 14:40:48 +0800

    Initial commit

---
+++ graph/work_graph.py
@@ -0,0 +1,399 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import time
+import threading
+import sys
+import logging
+
+import pickle
+import datetime
+import commands as cmd
+
+time_format = datetime.datetime.now().strftime('%Y-%m-%d')
+
+
+class WorkNode(object):
+
+    def __init__(self, script_log_path):
+        self.node_name = None
+        self.description = None
+        self.next_nodes = set()
+        self.last = set()
+        self.status = 0
+        self.restart_times = 2
+        self.script_log_path = script_log_path
+        pass
+
+    def get_log_name(self):
+        """
+        生成log文件名称
+        :return:
+        """
+        log_name = self.node_name.split(u"/")[-1].split(u".")[0] + "." + time_format
+        return log_name
+
+    def set_restart(self, times=2):
+        self.restart_times = times
+
+    def set_name(self, name):
+        self.node_name = name
+
+    def add_next(self, next_node):
+        self.next_nodes.add(next_node)
+
+    def add_last(self, last_node):
+        self.last.add(last_node)
+
+    def set_status(self, status):
+        self.status = status
+
+    def start(self):
+        """
+        # 检查自己的状态, 自己的状态是未运行, 或者运行失败,  才可以运行,  已经运行和运行成功不能运行
+        # 将运行结果放到自己的状态, 单线程, 避免多线程出错
+        :return:
+        """
+        if self.check_self():
+            logging.info('主线程 for循环内 check_and_start; 子线程内WordNode准备开始运行 ->' + str(self.node_name))
+            self.status = Status.running
+            t = threading.Thread(target=self.run_script)
+            t.start()
+        pass
+
+    def run_script(self):
+        """
+        # 脚本运行线程内
+        :return:
+        """
+        log_name = self.get_log_name()
+        cmd_ = u"bash " + unicode(self.node_name) + u" > " \
+               + unicode(self.script_log_path) + u"/" + unicode(log_name) + u" 2>&1"
+        status, _ = cmd.getstatusoutput(cmd_)
+        logging.info('子线程内 -------------------------; WordNode, run_script运行结束 ->' + str(self.node_name)
+                     + "结束状态:" + str(status))
+        self.check_run_result(status)
+        time.sleep(1)
+
+    def check_run_result(self, run_status):
+        """
+        检查运行结果, 并且切换状态, 保证这段时间 状态数据只能自己访问
+        1, 先检查脚本返回状态, 状态=0 运行成功, 其他, 运行失败
+        2, 如果运行失败, 就重启吧
+        :param run_status:
+        :return:
+        """
+        if run_status == 0:
+            self.status = Status.run_success
+            logging.info('子线程内 -------------------------; WordNode, run_script运行成功!! ->' + str(self.node_name))
+        else:
+            if self.restart_times >= 0:
+                logging.warn('子线程内 -------------------------; WordNode, run_script运行失败!! 180后重新运行 ->'
+                             + str(self.node_name))
+                self.restart_times -= 1
+                time.sleep(ProjectTime.restart_interval)
+                logging.warn('子线程内 -------------------------; WordNode, run_script重新运行!! ->' + str(self.node_name))
+                self.run_script()
+            else:
+                self.status = Status.run_failed
+
+    def check_self(self):
+        """
+        1, 自我状态检查, 首页要保证重启次数还有,如果重启次数都用光了, 就不能启动
+        2, 运行失败和还未运行的状态才能重新启动, 运行成功和正在运行的状态不能启动
+        :return:
+        """
+        logging.info('主线程 for循环内 check_and_start; WordNode进行自我检查   ->' + str(self.node_name))
+        if self.restart_times >= 0:
+            if (self.status == Status.not_run) or (self.status == Status.run_failed):
+                logging.info('主线程 for循环内 check_and_start; WordNode进行自我检查:通过 ->' + str(self.node_name))
+                return True
+            else:
+                logging.info('主线程 for循环内 check_and_start; WordNode进行自我检查:失败 ->' + str(self.node_name))
+                return False
+        else:
+            logging.info('主线程 for循环内 check_and_start; WordNode进行自我检查: 重启失败了,工程要退出了 ->' + str(self.node_name))
+            return False
+
+
+class WorkGraph(object):
+
+    def __init__(self, config_path):
+        self.config_path = config_path
+        self.node_dict = dict()  # 名字和节点的对应表
+        self.script_log_path = None
+        pass
+
+    def set_script_log_path(self, script_log_path):
+        self.script_log_path = script_log_path
+
+    def check_file_exist(self):
+        """
+        检查所有的脚本文件是否存在
+        :return:
+        """
+        file_exist = True
+        import os
+        for node_path in self.node_dict:
+            if not os.path.exists(node_path):
+                file_exist = False
+        return file_exist
+
+    def read_config(self):
+        with codecs.open(self.config_path, encoding="utf-8") as conf:
+            for line in conf:
+                line = line.strip()
+                self_node = line.split(u":")[0].strip()
+                next_node = line.split(u":")[1].strip()
+                self.check_node_dict_next(self_node, next_node)
+                self.check_node_dict_last(self_node, next_node)
+
+    def read_config_2(self):
+        """
+        读取另一种格式的文件
+        脚本a:b,c
+        表示a依赖于b,c
+        """
+        with codecs.open(self.config_path, encoding="utf-8") as conf:
+            for line in conf:
+                line = line.strip()
+                if not line.startswith(u"#"):
+                    self_nodes = line.split(u":")[1].strip().split(u",")
+                    next_node = line.split(u":")[0].strip()
+                    for self_node in self_nodes:
+                        self_node = self_node.strip()
+                        self.check_node_dict_next(self_node, next_node)
+                        self.check_node_dict_last(self_node, next_node)
+
+    def check_node_dict_next(self, self_node, next_node):
+        if self_node in self.node_dict:
+            wn = self.node_dict[self_node]
+            wn.add_next(next_node)
+        else:
+            wn = WorkNode(self.script_log_path)
+            wn.set_name(self_node)
+            wn.add_next(next_node)
+            self.node_dict[self_node] = wn
+
+    def check_node_dict_last(self, self_node, next_node):
+        if next_node in self.node_dict:
+            wn = self.node_dict[next_node]
+            wn.add_last(self_node)
+        else:
+            wn = WorkNode(self.script_log_path)
+            wn.set_name(next_node)
+            wn.add_last(self_node)
+            self.node_dict[next_node] = wn
+        pass
+
+    def print_(self):
+        for node in self.node_dict:
+            print("-----------------")
+            print(node)
+            print("前面的节点" + str(self.node_dict[node].last))
+            print("后面的节点" + str(self.node_dict[node].next_nodes))
+            logging.info("-----------------\n")
+            logging.info(str(node))
+            logging.info("前面的节点" + str(self.node_dict[node].last))
+            logging.info("后面的节点" + str(self.node_dict[node].next_nodes))
+
+    def start_all_script(self, seconds=20):
+        """
+        整个工程的入口
+        1, 先检查所有的脚本文件是否存在, 不存在就返回
+        2, 存在就开始循环检查
+        3, WorkGraph职责, 负责检查上下文状态, 只要上下文状态满足要求, 就启动节点执行任务
+        4, WorkNode职责,  满足上下文状态就执行, 自己的状态自己检查和调整
+        5, 当所有节点都是成功状态的时候, 也会退出工程
+        :return:
+        """
+        if not self.check_file_exist():
+            logging.warn('主线程 for循环内:脚本文件不全')
+            return
+        logging.info('主线程:工程开始')
+        while True:
+            time.sleep(seconds)
+            self.print_all_status()
+            for node_name in self.node_dict:
+                time.sleep(0.1)
+                if self.node_dict[node_name].status != Status.run_success:
+                    logging.info('主线程 for循环内:开始检查如下节点    ->                                ' + str(node_name))
+                    logging.info('主线程 for循环内:当前节点状态       ->' + str(Status.name[self.node_dict[node_name].status]))
+                    self.check_and_start(node_name)
+                    logging.info('主线程 for循环内:开始检查如下节点结束 ->' + str(node_name) + "\n\n")
+
+            if self.all_success():
+                for _ in self.node_dict:
+                    logging.info('主线程 for循环内:所有节点运行成功')
+                break
+
+    def all_success(self):
+        """
+        检查所有节点都是不是成功状态
+        :return:
+        """
+        ok = True
+        for w in self.node_dict:
+            if self.node_dict[w].status != Status.run_success:
+                ok = False
+        return ok
+
+    def check_and_start(self, node_name):
+        """
+        开始运行node的代码入口在这里...........................................
+        1, 首先检查前面节点的状态: self.check_last_nodes_status(work_node)
+        2, 检查整个工程的状态, 如果有job挂了, 就记录状态, 停止整个工程, 当多次重新运行失败的时候, work_node才会把节点置成失败状态)
+        work_node: 当前工作节点
+        :param node_name: 当前节点名称
+        :return:
+        """
+        work_node = self.node_dict[node_name]
+        last_all_finish = self.check_last_nodes_status(work_node)
+        logging.info('主线程 for循环内 check_and_start: 当前节点状态    ->' + Status.name[work_node.status])
+        if last_all_finish:
+            logging.info('主线程 for循环内 check_and_start: 节点开始运行 ->' + str(work_node.node_name))
+            work_node.start()
+        else:
+            logging.info('主线程 for循环内 check_and_start: 节点等待    ->')
+
+        """如果有job挂了, 并且没有节点是运行状态, 并且重启次数全部用完, 就停止, 这里延时0.1秒等待 work_node.start() 成功改变了状态"""
+        time.sleep(0.1)
+
+        if self.check_failed_job() and self.check_running_node():
+            logging.info('主线程 for循环内 check_and_start: 节点等待 准备保存退出->')
+            logging.info('主线程 for循环内 check_and_start: 有job挂了, 脚本退出: check_failed_job_success')
+            '''
+            保存并且停止
+            '''
+            self.save_status()
+            sys.exit(0)
+
+    def check_running_node(self):
+        has_no_node_run = True
+        for key in self.node_dict:
+            node = self.node_dict[key]
+            if node.status == Status.running:
+                has_no_node_run = False
+        return has_no_node_run
+
+    def check_failed_job(self):
+        has_failed_job = False
+        for key in self.node_dict:
+            node = self.node_dict[key]
+            if node.status == Status.run_failed and node.restart_times < 0:
+                has_failed_job = True
+        return has_failed_job
+
+    def check_last_nodes_status(self, work_node):
+        logging.info('主线程 for循环内 check_last_nodes_status: 检查前面节点的状态' + str(work_node.node_name))
+        last_node_names = work_node.last
+        last_finish = True
+
+        for last_node_name in last_node_names:
+            last_node_status = self.node_dict[last_node_name].status
+            logging.info('主线程 for循环内 check_last_nodes_status: 前面节点的状态:'
+                         + str(last_node_name) + ":" + Status.name[last_node_status])
+            if last_node_status != Status.run_success:
+                last_finish = False
+        logging.info(
+            '主线程 for循环内 check_last_nodes_status: 前面节点的状态检查结果:' + str(last_finish))
+        return last_finish
+
+    def save_status(self, file_path=None):
+        """
+        将现在的节点状态保存起来
+        :param file_path:
+        :return:
+        """
+        if file_path is None:
+            file_path = time_format
+        output = open(file_path, 'wb')
+        pickle.dump(self.node_dict, output)
+        output.close()
+
+    def load_status(self, file_path=None):
+        """
+        载入节点状态
+        :param file_path:
+        :return:
+        """
+        if file_path is None:
+            file_path = time_format
+            pkl_file = open(file_path, 'rb')
+            self.node_dict = pickle.load(pkl_file)
+            pkl_file.close()
+
+    def print_all_status(self):
+        x = []
+        for key in self.node_dict:
+            node = self.node_dict[key]
+            x.append(str(key) + ":")
+            x.append(str(Status.name[node.status]) + ", ")
+        logging.info('主线程 for循环内:一次循环检查开始, 打印所有节点的状态  ------------>' + '\n' + str(x))
+
+    def restart(self, seconds=20):
+        cmd.getstatusoutput("mv " + "log." + time_format + " " + "log." + time_format + ".old")
+        self.load_status()
+        for key in self.node_dict:
+            node = self.node_dict[key]
+            node.set_restart()
+        logging.info('主线程 重启运行  ------------>' + '\n')
+        self.start_all_script(seconds)
+
+
+class Status(object):
+    not_run = 0
+    running = 1
+    run_success = 2
+    run_failed = 3
+    name = {0: "not_run", 1: "running", 2: "run_success", 3: "run_failed"}
+
+
+class ProjectTime(object):
+
+    restart_interval = 180  # 重启间隔
+
+
+if __name__ == "__main__":
+
+    check_interval = 60
+
+    """
+    第一个参数: 是配置文件路径
+    第二个参数: 运行的各个脚本log保存位置
+    第三个参数: 是否重启, false, 不重启
+    第四个参数: work_graph Log保存位置
+    """
+    work_graph_log_path = sys.argv[4]
+
+    logging.basicConfig(level=logging.INFO,
+                        filename=work_graph_log_path + "/work_graph_log." + time_format,
+                        filemode='w',
+                        format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
+    path = sys.argv[1]
+    script_log_p = sys.argv[2]
+    is_restart = sys.argv[3]
+    # path = sys.argv[1]
+    # path = "./test_file"
+    wg = WorkGraph(path)
+    wg.set_script_log_path(script_log_p)
+    print script_log_p
+    wg.read_config_2()
+    wg.print_()
+    if is_restart == "false":
+        wg.start_all_script(check_interval)
+    elif is_restart == "true":
+        wg.restart(check_interval)
+
+    # wg = WorkGraph("test_file")
+    # wg.read_config_2()
+    # wg.print_()
+    pass
+
+
+"""
+还要实现的功能:
+1, 检查某hdfs几个文件有没有, 如果存在, 才启动脚本
+2, 打印每个节点的运行时间
+"""
